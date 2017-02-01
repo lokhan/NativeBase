0057b97bf6d25336fc34d27c4aa1df905573e53e
Lokhan Wong <lwong@binck.nl>
2017-02-01 19:19:09 +0100

    compiled sources

--- dist/Components/Widgets/Icon/index.js
+++ dist/Components/Widgets/Icon/index.js
@@ -11,7 +11,8 @@ var _FontAwesome=require('react-native-vector-icons/FontAwesome');var _FontAweso
 var _Foundation=require('react-native-vector-icons/Foundation');var _Foundation2=_interopRequireDefault(_Foundation);
 var _MaterialIcons=require('react-native-vector-icons/MaterialIcons');var _MaterialIcons2=_interopRequireDefault(_MaterialIcons);
 var _Octicons=require('react-native-vector-icons/Octicons');var _Octicons2=_interopRequireDefault(_Octicons);
-var _Zocial=require('react-native-vector-icons/Zocial');var _Zocial2=_interopRequireDefault(_Zocial);function _interopRequireDefault(obj){return obj&&obj.__esModule?obj:{default:obj};}function _classCallCheck(instance,Constructor){if(!(instance instanceof Constructor)){throw new TypeError("Cannot call a class as a function");}}function _possibleConstructorReturn(self,call){if(!self){throw new ReferenceError("this hasn't been initialised - super() hasn't been called");}return call&&(typeof call==="object"||typeof call==="function")?call:self;}function _inherits(subClass,superClass){if(typeof superClass!=="function"&&superClass!==null){throw new TypeError("Super expression must either be null or a function, not "+typeof superClass);}subClass.prototype=Object.create(superClass&&superClass.prototype,{constructor:{value:subClass,enumerable:false,writable:true,configurable:true}});if(superClass)Object.setPrototypeOf?Object.setPrototypeOf(subClass,superClass):subClass.__proto__=superClass;}var
+var _Zocial=require('react-native-vector-icons/Zocial');var _Zocial2=_interopRequireDefault(_Zocial);
+var _MaterialCommunityIcons=require('react-native-vector-icons/MaterialCommunityIcons');var _MaterialCommunityIcons2=_interopRequireDefault(_MaterialCommunityIcons);function _interopRequireDefault(obj){return obj&&obj.__esModule?obj:{default:obj};}function _classCallCheck(instance,Constructor){if(!(instance instanceof Constructor)){throw new TypeError("Cannot call a class as a function");}}function _possibleConstructorReturn(self,call){if(!self){throw new ReferenceError("this hasn't been initialised - super() hasn't been called");}return call&&(typeof call==="object"||typeof call==="function")?call:self;}function _inherits(subClass,superClass){if(typeof superClass!=="function"&&superClass!==null){throw new TypeError("Super expression must either be null or a function, not "+typeof superClass);}subClass.prototype=Object.create(superClass&&superClass.prototype,{constructor:{value:subClass,enumerable:false,writable:true,configurable:true}});if(superClass)Object.setPrototypeOf?Object.setPrototypeOf(subClass,superClass):subClass.__proto__=superClass;}var
 
 Icon=function(_NativeBaseComponent){_inherits(Icon,_NativeBaseComponent);function Icon(){_classCallCheck(this,Icon);return _possibleConstructorReturn(this,(Icon.__proto__||Object.getPrototypeOf(Icon)).apply(this,arguments));}_createClass(Icon,[{key:'componentWillMount',value:function componentWillMount()
 
@@ -23,8 +24,10 @@ Icon=function(_NativeBaseComponent){_inherits(Icon,_NativeBaseComponent);functio
 
 
 
+
 {
-switch(this.getTheme().iconFamily){
+var iconFamily=this.props.iconFamily?this.props.iconFamily:this.getTheme().iconFamily;
+switch(iconFamily){
 case'Ionicons':
 this.Icon=_Ionicons2.default;
 break;
@@ -46,6 +49,8 @@ break;
 case'Zocial':
 this.Icon=_Zocial2.default;
 break;
+case'MaterialCommunity':
+this.Icon=_MaterialCommunityIcons2.default;
 default:
 this.Icon=_Ionicons2.default;}
 
